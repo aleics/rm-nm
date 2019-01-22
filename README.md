198743ca1fc49abb98e28a408dee07b13c45199d
Aleix Casanovas <aleix.casanovas@leanix.net>
2019-01-22 17:16:48 +0100

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# rm-nm
+Small tool written in Rust to remove all `node_modules` from a directory.
---
+++ src/main.rs
@@ -0,0 +1,127 @@
+extern crate clap;
+use clap::{App, load_yaml};
+use std::{io, fs, env};
+use std::path::PathBuf;
+
+fn main() {
+    let cli = load_yaml!("cli.yml");
+    let matches = App::from_yaml(cli).get_matches();
+
+    let recursive = matches.is_present("recursive");
+    let directory = extract_directory(matches.value_of("directory")).unwrap();
+
+    let results = if recursive {
+        rm_modules_rec( &directory)
+    } else {
+        vec![rm(&directory)]
+    };
+
+    for result in results {
+        match result {
+            Ok(p) => println!("removed successfully in {}", p.display()),
+            Err(err) => println!("{}", err)
+        }
+    }
+}
+
+fn extract_directory(arg_directory: Option<&str>) -> io::Result<PathBuf> {
+    arg_directory.map_or_else(|| env::current_dir(), |d| {
+        let mut path = PathBuf::new();
+        path.push(d);
+
+        if !path.exists() {
+            Err(
+                io::Error::new(io::ErrorKind::NotFound, format!("directory {:?} not found.", path.display()))
+            )
+        } else {
+            Ok(path)
+        }
+    })
+}
+
+fn rm_modules_rec(path: &PathBuf) -> Vec<io::Result<PathBuf>> {
+    let mut vec: Vec<io::Result<PathBuf>> = Vec::new();
+    // remove node modules in the current path
+    vec.push(rm(path));
+
+    let entries = path.read_dir().expect("couldn't read directory entries");
+    for entry in entries {
+        if let Ok(entry) = entry {
+            vec.append(&mut rm_modules_rec(&entry.path()));
+        }
+    }
+
+    vec
+}
+
+fn rm(path: &PathBuf) -> io::Result<PathBuf> {
+    let mut modules_path = path.clone();
+    modules_path.push("node_modules");
+    if !(modules_path.exists() && modules_path.is_dir())  {
+        Err(
+            io::Error::new(io::ErrorKind::NotFound, format!("directory {:?} not found.", modules_path.display()))
+        )
+    } else {
+        fs::remove_dir_all(modules_path).map(|_| path.clone())
+    }
+}
+
+mod tests {
+    use super::*;
+
+    fn prepare_dir<T>(test: T) -> ()
+        where T: Fn(PathBuf) -> () {
+        let mut current = env::current_dir().unwrap();
+        current.push("node_modules");
+
+        if !current.exists() {
+            fs::create_dir(current.clone()).expect("should create a node_modules directory");
+        }
+
+        // set to initial current directory
+        current.pop();
+
+        test(current.clone());
+
+        current.push("node_modules");
+        if current.exists() {
+            fs::remove_dir(current.clone()).expect("should remove a node_modules directory");
+        }
+    }
+
+    #[test]
+    fn test_extract_current_directory() {
+        let directory = extract_directory(None).unwrap();
+        assert_eq!(directory, env::current_dir().unwrap());
+    }
+
+    #[test]
+    fn test_extract_some_non_existing_directory() {
+        assert!(extract_directory(Some("/some/directory")).is_err());
+    }
+
+    #[test]
+    fn test_extract_some_existing_directory() {
+        let current = env::current_dir().unwrap();
+        let path = extract_directory(current.to_str()).unwrap();
+        assert_eq!(path, current);
+    }
+
+    #[test]
+    fn test_rm_dir() {
+        prepare_dir(|current_dir| {
+            match rm(&current_dir) {
+                Ok(result) => assert_eq!(result, current_dir),
+                Err(e) => assert!(false, format!("Error {:?}", e))
+            }
+        });
+    }
+
+    #[test]
+    fn test_rm_non_existing_dir() {
+        let mut dir = PathBuf::new();
+        dir.push("/some/directory");
+
+        assert!(rm(&dir).is_err());
+    }
+}
